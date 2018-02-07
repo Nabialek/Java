277132e61f075caf8b034f86a343a19dfa3506f5
Pawel <nabialekpawel@gmail.com>
2018-02-07 23:23:39 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+        int age = 20;
+        System.out.println(age);
+    }
+}
